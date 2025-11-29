544dbffeb8b3029cd6e84e4b2688fbce7a314234
Ramosjhonatan-test <ramosjhonatan659@gmail.com>
2025-11-29 00:17:35 -0400

    s

--- start.sh
+++ start.sh
@@ -6,5 +6,8 @@ if ! command -v ffmpeg >/dev/null 2>&1; then
   apt-get update && apt-get install -y ffmpeg
 fi
 
+# Instala dependencias Python
+pip install -r requirements.txt
+
 # Arranca el servidor
 python server_wss.py
